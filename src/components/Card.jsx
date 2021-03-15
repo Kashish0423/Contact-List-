7235dbaaa0c9d4dee4827038c2b55b337dd18858
Kashish Choubey <kashishchoubey7516@gmail.com>
2021-03-15 23:28:41 +0530

    Contact list 1.1

---
+++ src/components/Card.jsx
@@ -0,0 +1,66 @@
+import React from "react" ;
+
+
+
+function Temp(props){
+
+
+    return (
+        <div >
+         
+          <div className="card">
+            <div className="top">
+              <h2 className="name">{props.name}</h2>
+              <img
+               className="circle-img" src={props.img}
+                alt="avatar_img"
+              />
+            </div>
+            <div className="bottom">
+              <p className="info">{props.contact}</p>
+              <p className="info">{props.email}</p>
+            </div>
+          </div>
+        </div>
+      );
+
+}
+
+
+
+
+function Card(){
+
+   return ( <div>
+
+    <h1 className="heading">My Contacts</h1>
+    
+         <Temp name= "Beyonce" img= "https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg" contact="+123 456 789"
+    
+       email= "b@beyonce.com"
+    
+         />
+    
+    <Temp name= "Jack Bauer" img= "https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg" contact="+987 654 321"
+    
+    email= "jack@nowhere.com"
+    
+      />
+    
+    <Temp name= "Chuck Norris" img= "https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png" contact="+918 372 574"
+    
+    email= "gmail@chucknorris.com"
+    
+      />
+    
+    
+        </div>
+        
+   ) ;
+
+
+
+}
+
+
+export default Card ;
